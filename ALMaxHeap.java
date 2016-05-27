eb0de2b31524e27a7acb90571fd29b6d74532c29
Jason <Jason>
2016-05-26 22:25:45 -0400

    added size method

--- ALMaxHeap.java
+++ ALMaxHeap.java
@@ -188,6 +188,8 @@ public class ALMaxHeap {
     private void swap( int pos1, int pos2 ) {
 	_heap.set( pos1, _heap.set( pos2, _heap.get(pos1) ) );	
     }
+
+    public int size() { return _heap.size(); }
     //********************************************
 
 
